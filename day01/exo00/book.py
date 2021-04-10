9bb2a40f5a375504c9adab173d558da7e9103ace
africain30 <80362737+africain30@users.noreply.github.com>
2021-04-10 21:14:23 +0100

    Create book.py

---
+++ day01/exo00/book.py
@@ -0,0 +1,44 @@
+import datetime
+from recipe import Recipe
+class book:
+       def __init__(self,name ,last_update ,creation_date ,recipes_list):
+           if type(name)!=str or name=="":
+               raise ValueError
+           self.name = name
+           
+           self.last_update=last_update
+           self.creation_date=creation_date
+           if type(recipes_list)!=dict or "starter" not in recipe_list or "lunch"not in recipe_list or "dessert" not in recipe_list:
+               raise ValueError
+           self.recipe_list
+    
+      def get_recipe_by_name(self, name):
+          starter=list(self.recipe_list["starter"])
+          lunch=list(self.recipe_list["lunch"])
+          dessert=list(self.recipe_list["dessert"])
+          if name in starter:
+              return self.recipe_list["starter"][name]
+          elif name in lunch:
+              return self.recipe_list["lunch"][name]
+          elif name in starter:
+              return self.recipe_list["desert"][name] 
+          else:
+              return "their is no recipe with this name!"
+          
+
+    def get_recipes_by_types(self, recipe_type):
+         if recipe_type=="starter"
+             return self.recipe_list["starter"]
+         elif recipe_type=="lunch"
+             return self.recipe_list["lunch"]
+         elif recipe_type=="desert"
+             return self.recipe_list["desert"]
+         else:
+             raise "their is no recipe with this type"
+             
+             
+    def add_recipe(self, recipe):
+        if type(recipe)!= Recipe :
+            return " Before adding a recipe , please create one "
+        self.recipe_list[recipe.recipe_type][recipe.recipe_name]=recipe
+        self.last_update = datetime.datetime.now()

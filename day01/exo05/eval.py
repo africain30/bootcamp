e4d5733054cb792f342a8428cdce579be8bfc129
africain30 <80362737+africain30@users.noreply.github.com>
2021-04-12 11:14:05 +0000

    Update eval.py

--- day01/exo05/eval.py
+++ day01/exo05/eval.py
@@ -1,17 +1,33 @@
 class EVALUATOR:
+    
+    
     def zip_evaluator(words,coeffs):
         if type(words)!=list or type(coeffs)!=list:
             raise TypeError
+            
         if len(words)!=len(coeffs):
-            raise ValueError
-        for i in words:
+            raise -1
+        
+        words1=list(words)
+        for i in words1:
             if type(i)!=str :
                 raise TypeError
-        coeffs=tuple(coeffs)
+        coeffs1list(coeffs)       
         for i in coeffs:
-            if type(coeffs[i])!= int and type(coeffs[i])!=float:
+            if type(coeffs1[i])!= int and type(coeffs1[i])!=float:
                 raise TypeError
                 
+        value = 0;
+        for i in range(len(words1)):
+            for j in range(len(words1[i])):
+                x+= coeffs1[i]*words1[i][j]
+        return value
+        
         
         
     def enumerate_evaluate(words,coeffs):
+        if type(words)!=list or type(coeffs)!=list:
+            raise TypeError
+            
+        if len(words)!=len(coeffs):
+            raise -1
